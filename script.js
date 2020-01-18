f4b995ba73dfef128c09f6ec90fe3133ed4e5964
fran <francisco.chiarino@gmail.com>
2020-01-18 19:01:28 +0100

    Made it until turnCardsBack function!

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // Variables
 // Store the HTML collection in 'cards':
 const cards = document.getElementsByClassName('flip-card-inner');
-// Set up a counter to only let the player turn the cards twice:
-let counter = 0;
-// Let the script know which images are being turned (these variables are gonna carry the img's class):
+// Let the script know which images are being turned (these variables are gonna carry the img's id):
 let turnedCards = [];
 
 
@@ -13,19 +11,51 @@ let turnedCards = [];
 const cardTurner = (id) => {
     id.style.transform = `rotateY(180deg)`;
     // Push each card to turned cards array:
-    turnedCards.push(id.attributes[0].textContent.split(' ')[1]);
+    turnedCards.push(id);
+    // turnedCards.push(id.attributes[0].textContent.split(' ')[1]);
 }
 
+const checkMatchingPairs = () => {
+    // Store each card's class into variables:
+    let cardClass1 = turnedCards[0].attributes[0].textContent.split(' ')[1];
+    let cardClass2 = turnedCards[1].attributes[0].textContent.split(' ')[1];
+    // Compare them:
+    if (cardClass1 === cardClass2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const turnCardsBack = () => {
+    console.log('Hi there');
+    
+}
 
 // Apply click event listener for all cards:
 for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener('click', function() {
         let id = document.getElementById(`${cards[i].id}`)
-        // Run functions:
+
+        // Turn card:
         cardTurner(id);
 
+        // If two cards are turned, compare them:
+        if (turnedCards.length === 2) {
+
+            // If checkMatchingPairs returns true, then the cards are goint to keep facing up.
+            checkMatchingPairs();
+
+            // If its false, then the cards are going to turn back after 2 seconds:
+            if (checkMatchingPairs() === false) {
+                setTimeout(() => {
+                    console.log("World!"); 
+                    turnCardsBack();
+                }, 2000);
+            }
+        }
+        
     })
 }
 
-console.log(cards)
 
