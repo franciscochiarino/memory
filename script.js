3b60b5cd2fa9a1c0b7dd163c97e3df3fe899a23c
fran <francisco.chiarino@gmail.com>
2020-01-18 18:19:49 +0100

    Try pushing turned cards to an array

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
- // Variables
- const cat = document.getElementById('cat');
- let card = document.getElementsByClassName('flip-card-inner');
-
- // If both cards have the same class, then that means they have the same image. Here is how you know: <id>.attributes[0].textContent.split(' ')[1]; 
-
- // Make the card turn after click
- const cardTurner = (id) => {
-     id.style.transform = `rotateY(180deg)`;
-     console.log(id)
-    //  text = cat.attributes[0].textContent.split(' ')[1];
-    }
+// Variables
+// Store the HTML collection in 'cards':
+const cards = document.getElementsByClassName('flip-card-inner');
+// Set up a counter to only let the player turn the cards twice:
+let counter = 0;
+// Let the script know which images are being turned (these variables are gonna carry the img's class):
+let turnedCards = [];
 
 
- // Event listener
-//  card.addEventListener('click', function() {
-//      cardTurner(cat);
-//     });
+// If both cards have the same class, then that means they have the same image. Here is how you know: <id>.attributes[0].textContent.split(' ')[1]; 
 
-for (let i = 0; i < card.length; i++) {
-    card[i].addEventListener('click', function() {
-        let id = document.getElementById(`${card[i].id}`)
+// Make the card turn after click
+const cardTurner = (id) => {
+    id.style.transform = `rotateY(180deg)`;
+    // Push each card to turned cards array:
+    turnedCards.push(id.attributes[0].textContent.split(' ')[1]);
+}
+
+
+// Apply click event listener for all cards:
+for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('click', function() {
+        let id = document.getElementById(`${cards[i].id}`)
+        // Run functions:
         cardTurner(id);
+
     })
 }
+
+console.log(cards)
 
