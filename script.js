7076c454de4a8fc0d2c4d54bf48f16168d3822e4
fran <francisco.chiarino@gmail.com>
2020-01-18 16:25:07 +0100

    Loaded images to html

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
  // Variables
  const cat = document.getElementById('cat');
- degCounter = 0;
+ let card = document.getElementsByClassName('flip-card-inner');
 
  // If both cards have the same class, then that means they have the same image. Here is how you know: <id>.attributes[0].textContent.split(' ')[1]; 
 
  // Make the card turn after click
  const cardTurner = (id) => {
-     degCounter += 180;
-     id.style.transform = `rotateY(${degCounter}deg)`;
+     id.style.transform = `rotateY(180deg)`;
+     console.log(id)
     //  text = cat.attributes[0].textContent.split(' ')[1];
-    console.log('dick')
     }
 
 
  // Event listener
- cat.addEventListener('click', function() {
-     cardTurner(cat);
-    });
+//  card.addEventListener('click', function() {
+//      cardTurner(cat);
+//     });
+
+for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener('click', function() {
+        let id = document.getElementById(`${card[i].id}`)
+        cardTurner(id);
+    })
+}
+
